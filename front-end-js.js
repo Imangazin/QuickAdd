4b8644395f756770e43efb09ee01abda55122434
Nurbek Imangazin <39064313+Imangazin@users.noreply.github.com>
2023-01-12 11:25:34 -0500

    Add files via upload

---
+++ front-end-js.js
@@ -0,0 +1,11 @@
+<div id="quickAddContainer">
+//add update the src parameter with quicklink to LTI tool.
+<iframe src="" style="overflow-y: hidden; height: 260px; width: 100%;" scrolling="no" frameborder="0"> </iframe>
+</div>
+<script>
+window.addEventListener('load', function () { 
+  if ({RoleId}!=109 && {RoleId}!=105){ document.getElementById('quickAddContainer').parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display='none'; //Remove this widget 
+  } 
+})
+</script>
+</p>
