1b43beb0f54d1f6e3efe3c1bd8f36e048d234e58
Nurbek Imangazin <39064313+Imangazin@users.noreply.github.com>
2023-01-13 09:05:39 -0500

    Update front-end-js.js

--- front-end-js.js
+++ front-end-js.js
@@ -1,11 +1,12 @@
 <div id="quickAddContainer">
-//add update the src parameter with quicklink to LTI tool.
-<iframe src="" style="overflow-y: hidden; height: 260px; width: 100%;" scrolling="no" frameborder="0"> </iframe>
 </div>
 <script>
 window.addEventListener('load', function () { 
   if ({RoleId}!=109 && {RoleId}!=105){ document.getElementById('quickAddContainer').parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display='none'; //Remove this widget 
-  } 
+  } else {
+    //add update the src parameter with quicklink to LTI tool.
+    document.getElementById('quickAddContainer').innerHTML = '<iframe src="" style="overflow-y: hidden; height: 260px; width: 100%;" scrolling="no" frameborder="0"> </iframe>';
+  }
 })
 </script>
 </p>
