00313fe6536e63ef67952ce55b56efde2e469577
Nurbek Imangazin <39064313+Imangazin@users.noreply.github.com>
2023-01-16 10:52:06 -0500

    Update front-end-js.js

--- front-end-js.js
+++ front-end-js.js
@@ -2,7 +2,7 @@
 
 <script>
 window.addEventListener('load', function () { 
-  if ({RoleId}==109 || {RoleId}==105){document.getElementById('quickAddContainer').innerHTML = '<iframe src="/d2l/common/dialogs/quickLink/quickLink.d2l?ou={orgUnitId}&amp;type=lti&amp;rcode=E3441D29-BE5A-4BE7-AA7C-8C696DC9DDED-80125&amp;srcou=6606" style="overflow-y: hidden; height: 260px; width: 100%;" scrolling="no" frameborder="0"> </iframe>';    
+  if ({RoleId}==109 || {RoleId}==105){document.getElementById('quickAddContainer').innerHTML = '<iframe src="" style="overflow-y: hidden; height: 260px; width: 100%;" scrolling="no" frameborder="0"> </iframe>';    
   } else { 
 document.getElementById('quickAddContainer').parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display='none'; //Remove this widget 
   }
