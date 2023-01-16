1346d11c3949515c3b8e3ca292d3358da505d2c5
Nurbek Imangazin <39064313+Imangazin@users.noreply.github.com>
2023-01-16 09:06:56 -0500

    Update front-end-js.js

--- front-end-js.js
+++ front-end-js.js
@@ -1,12 +1,11 @@
-<div id="quickAddContainer">
-</div>
+<div id="quickAddContainer"></div>
+
 <script>
 window.addEventListener('load', function () { 
-  if ({RoleId}!=109 && {RoleId}!=105){ document.getElementById('quickAddContainer').parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display='none'; //Remove this widget 
-  } else {
-    //add update the src parameter with quicklink to LTI tool.
-    document.getElementById('quickAddContainer').innerHTML = '<iframe src="" style="overflow-y: hidden; height: 260px; width: 100%;" scrolling="no" frameborder="0"> </iframe>';
+  if ({RoleId}==109 || {RoleId}==105){document.getElementById('quickAddContainer').innerHTML = '<iframe src="/d2l/common/dialogs/quickLink/quickLink.d2l?ou={orgUnitId}&amp;type=lti&amp;rcode=E3441D29-BE5A-4BE7-AA7C-8C696DC9DDED-80125&amp;srcou=6606" style="overflow-y: hidden; height: 260px; width: 100%;" scrolling="no" frameborder="0"> </iframe>';    
+  } else { 
+document.getElementById('quickAddContainer').parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display='none'; //Remove this widget 
   }
 })
 </script>
-</p>
+
