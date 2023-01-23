4049847b26aff117402e66c3493cb5d4f36127e1
Nurbek Imangazin <39064313+Imangazin@users.noreply.github.com>
2023-01-23 16:33:21 -0500

    Update front-end-js.js

--- front-end-js.js
+++ front-end-js.js
@@ -1,11 +1,20 @@
-<div id="quickAddContainer"></div>
-
-<script>
-window.addEventListener('load', function () { 
-  if ({RoleId}==109 || {RoleId}==105){document.getElementById('quickAddContainer').innerHTML = '<iframe src="" style="overflow-y: hidden; height: 260px; width: 100%;" scrolling="no" frameborder="0"> </iframe>';    
-  } else { 
+function quickAddVisibility(roleId, orgUnitId){
+	// populate src parameter of the iframe with quicklink to the LTI tool, and replace within the link {OrgUnitId} with '+orgUnitId+'
+	if (roleId==109 || roleId==105){document.getElementById('quickAddContainer').innerHTML = '<iframe src="" style="overflow-y: hidden; height: 260px; width: 100%;" scrolling="no" frameborder="0"> </iframe>';    
+    } else { 
 document.getElementById('quickAddContainer').parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display='none'; //Remove this widget 
-  }
+      }
+}
+/*
+//The following code should be placed into widget
+
+<div id="quickAddContainer"></div>
+//populate src parameter with path to this js file, you can place it in BS public files folder
+<script type="text/javascript" src=""></script>
+<script>
+window.addEventListener('load', function () {
+  quickAddVisibility({RoleId}, {OrgUnitId});
 })
 </script>
+*/
 
