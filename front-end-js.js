318e645bbadb03109d053a263224b6ba2b614732
Nurbek Imangazin <nurbekimangazin@gmail.com>
2024-03-27 10:21:08 -0400

    test

--- front-end-js.js
+++ front-end-js.js
@@ -3,10 +3,9 @@
 
 <div id="quickAddContainer"></div>
 //populate src parameter with path to this js file, you can place it in BS public files folder
-<script type="text/javascript" src=""></script>
 <script>
 window.addEventListener('load', function () {
-  if (roleId==109 || roleId==105 || roleId==){
+  if ({roleId}==109 || {roleId}==105){
     document.getElementById('quickAddContainer').innerHTML = '<iframe src="" style="overflow-y: hidden; height: 260px; width: 100%;" scrolling="no" frameborder="0"> </iframe>';    
   } else { 
     document.getElementById('quickAddContainer').parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display='none'; //Remove this widget 
